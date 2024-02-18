5b4cc12a0a043dd3343c20ae3de6fab8130611a4
Michael Hsu <michael100.hsu@gmail.com>
2024-02-18 12:55:35 -0600

    Finish extra models tutorial

--- src/tutorial/response_pydantic_models.py
+++ src/tutorial/response_pydantic_models.py
@@ -1,7 +1,9 @@
 """
 HTTP Responses with Pydantic Models
 https://fastapi.tiangolo.com/tutorial/response-model/
+https://fastapi.tiangolo.com/tutorial/extra-models/
 """
+from typing import Union
 from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr
 
@@ -78,3 +80,71 @@ class UserIn(BaseUser):
 @router.post("/inheritance/")
 async def create_user(user: UserIn) -> BaseUser:
     return user
+
+
+# Level 4: input, output, and database models
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str) -> str:
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn) -> UserInDB:
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@router.post("/in_out_db_models/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+
+# Response is the union of types
+
+class BaseItem(BaseModel):
+    description: str
+    type: str
+
+
+class CarItem(BaseItem):
+    type: str = "car"
+
+
+class PlaneItem(BaseItem):
+    type: str = "plane"
+    size: int
+
+
+items = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
+
+
+@router.get("/union_types/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+    return items[item_id]
