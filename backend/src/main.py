a5a49a36a740f6217dbdcf2ef9469663fb938ca0
Michael Hsu <michael100.hsu@gmail.com>
2024-03-25 12:41:22 -0500

    remove tutorial code

--- backend/src/main.py
+++ backend/src/main.py
@@ -16,17 +16,6 @@ from src.account.api_v1 import account_v1_router
 from src.auth.api_v1 import auth_v1_router
 from src.core.config import settings
 from src.core.constants import Environment, TestEnv
-from src.tutorial import (
-    cookie_header_router,
-    dependencies_router,
-    error_handling_router,
-    path_params_router,
-    query_params_router,
-    request_body_router,
-    response_pydantic_router,
-    status_codes_router,
-    validations_router,
-)
 
 if (
     os.getenv("ENVIRONMENT") == Environment.TESTING
@@ -73,46 +62,3 @@ async def validation_error_exception_handler(request: Request, exc: ValidationEr
 
 app.include_router(auth_v1_router)
 app.include_router(account_v1_router)
-
-app.include_router(path_params_router)
-app.include_router(query_params_router)
-app.include_router(request_body_router)
-app.include_router(validations_router)
-app.include_router(cookie_header_router)
-app.include_router(response_pydantic_router)
-app.include_router(status_codes_router)
-app.include_router(error_handling_router)
-app.include_router(dependencies_router)
-
-
-"""
-Error Handling
-https://fastapi.tiangolo.com/tutorial/handling-errors/
-"""
-
-
-# Custom error handlers
-class UnicornException(Exception):
-    def __init__(self, name: str):
-        self.name = name
-
-
-@app.exception_handler(UnicornException)
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
-    return JSONResponse(
-        status_code=418,
-        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
-    )
-
-
-@app.get("/unicorns/{name}")
-async def read_unicorn(name: str):
-    if name == "yolo":
-        raise UnicornException(name=name)
-    return {"unicorn_name": name}
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    print(f"OMG! The client sent invalid data!: {exc}")
-    return await request_validation_exception_handler(request, exc)
--- backend/src/tutorial/form_and_files.py
+++
@@ -1,6 +0,0 @@
-"""
-TODO
-Form Data - https://fastapi.tiangolo.com/tutorial/request-forms/
-Request Files - https://fastapi.tiangolo.com/tutorial/request-files/
-Request Forms and Files - https://fastapi.tiangolo.com/tutorial/request-forms-and-files/
-"""
